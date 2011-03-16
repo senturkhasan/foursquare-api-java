0ceb6a0ab496c0982e6618010e3c684b3e998120
antti.leppa@foyt.fi <antti.leppa@foyt.fi@3510f92a-767f-308a-6afb-0984ff0fccdf>
2011-03-16 04:26:01 +0000

    Version 0.11

---
+++ tags/v0.11/foursquare-api/src/fi/foyt/foursquare/api/entities/CompleteVenue.java
@@ -0,0 +1,66 @@
+/*
+ * FoursquareAPI - Foursquare API for Java
+ * Copyright (C) 2008 - 2011 Antti Leppä / Foyt
+ * http://www.foyt.fi
+ * 
+ * License: 
+ * 
+ * Licensed under GNU Lesser General Public License Version 2.1 or later (the "LGPL") 
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+
+package fi.foyt.foursquare.api.entities;
+
+public class CompleteVenue extends CompactVenue {
+
+  public String getDescription() {
+    return description;
+  }
+  
+  public Mayor getMayor() {
+    return mayor;
+  }
+  
+  public Tips getTips() {
+    return tips;
+  }
+  
+  public CompleteTodos getTodos() {
+    return todos;
+  }
+  
+  public String[] getTags() {
+    return tags;
+  }
+  
+  public Count getBeenHere() {
+    return beenHere;
+  }
+  
+  public String getShortUrl() {
+    return shortUrl;
+  }
+  
+  public CompleteSpecial[] getSpecialsNearby() {
+    return specialsNearby;
+  }
+  
+  public Photos getPhotos() {
+    return photos;
+  }
+  
+  public String getTimeZone() {
+    return timeZone;
+  }
+
+  private String description;
+  private Mayor mayor;
+  private Tips tips;
+  private CompleteTodos todos;
+  private String[] tags;
+  private Count beenHere;
+  private String shortUrl;
+  private CompleteSpecial[] specialsNearby;
+  private Photos photos;
+  private String timeZone;
+}
